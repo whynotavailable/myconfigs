029783d5a39544856b2617749bf450da9366ffb1
Tom <josephdotson@belkonar.com>
2025-01-22 18:18:22 -0500

    small updates

--- nvim/lua/plugins/main.lua
+++ nvim/lua/plugins/main.lua
@@ -4,53 +4,9 @@ return {
 		opts = {},
 	}, -- lazy
 	{
-		"catppuccin/nvim",
-		lazy = true,
-		name = "catppuccin",
-		opts = {
-			flavor = "mocha",
-			integrations = {
-				aerial = true,
-				alpha = true,
-				cmp = true,
-				dashboard = true,
-				flash = true,
-				grug_far = true,
-				gitsigns = true,
-				headlines = true,
-				illuminate = true,
-				indent_blankline = { enabled = true },
-				leap = true,
-				lsp_trouble = true,
-				mason = true,
-				markdown = true,
-				mini = true,
-				native_lsp = {
-					enabled = true,
-					underlines = {
-						errors = { "undercurl" },
-						hints = { "undercurl" },
-						warnings = { "undercurl" },
-						information = { "undercurl" },
-					},
-				},
-				navic = { enabled = true, custom_bg = "lualine" },
-				neotest = true,
-				neotree = true,
-				noice = true,
-				notify = true,
-				semantic_tokens = true,
-				telescope = true,
-				treesitter = true,
-				treesitter_context = true,
-				which_key = true,
-			},
-		},
-	},
-	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "catppuccin",
+			colorscheme = "carbonfox",
 		},
 	},
 	{
@@ -82,6 +38,7 @@ return {
 					"rust",
 					"templ",
 					"go",
+					"gdscript",
 				},
 				sync_install = false,
 				highlight = { enable = true },
@@ -90,44 +47,11 @@ return {
 		end,
 	},
 	{
-		"nvimtools/none-ls.nvim",
-		optional = true,
-		opts = function(_, opts)
-			local nls = require("null-ls")
-			opts.sources = opts.sources or {}
-			table.insert(opts.sources, nls.builtins.formatting.csharpier)
-		end,
-	},
-	{
-		"stevearc/conform.nvim",
-		optional = true,
-		opts = {
-			formatters_by_ft = {
-				cs = { "csharpier" },
-			},
-			formatters = {
-				csharpier = {
-					command = "dotnet-csharpier",
-					args = { "--write-stdout" },
-				},
-			},
-		},
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		-- enabled = false,
-	},
-	{
-		"folke/noice.nvim",
-		enabled = false,
-	},
-	{
 		"neovim/nvim-lspconfig",
 		opts = {
 			servers = {
 				html = {},
-				templ = {},
-				zls = {},
+				gdscript = {},
 			},
 			inlay_hints = {
 				enabled = false,
@@ -146,49 +70,8 @@ return {
 			formatters_by_ft = {
 				["markdown"] = { "mdformat" },
 				["python"] = { "isort" },
+				["cpp"] = { "clang-format" },
 			},
 		},
-	},
-	{
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		opts = {
-			menu = {
-				width = vim.api.nvim_win_get_width(0) - 4,
-			},
-			settings = {
-				save_on_toggle = true,
-			},
-		},
-		keys = function()
-			local keys = {
-				{
-					"<leader>H",
-					function()
-						require("harpoon"):list():add()
-					end,
-					desc = "Harpoon File",
-				},
-				{
-					"<leader>h",
-					function()
-						local harpoon = require("harpoon")
-						harpoon.ui:toggle_quick_menu(harpoon:list())
-					end,
-					desc = "Harpoon Quick Menu",
-				},
-			}
-
-			for i = 1, 5 do
-				table.insert(keys, {
-					"<C-" .. i .. ">",
-					function()
-						require("harpoon"):list():select(i)
-					end,
-					desc = "Harpoon to File " .. i,
-				})
-			end
-			return keys
-		end,
 	},
 }
