0da17928c793c0c2623c831c7a7b550623bb6128
Tom <josephdotson@belkonar.com>
2025-01-23 07:29:53 -0500

    final updates for now

--- nvim/lua/plugins/main.lua
+++ nvim/lua/plugins/main.lua
@@ -11,34 +11,34 @@ return {
 	-- Actual proper configs
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
-
-			---@diagnostic disable-next-line: missing-fields
-			configs.setup({
-				ensure_installed = {
-					"c_sharp",
-					"lua",
-					"vim",
-					"vimdoc",
-					"query",
-					"elixir",
-					"heex",
-					"javascript",
-					"html",
-					"angular",
-					"typescript",
-					"css",
-					"scss",
-					"markdown",
-					"rust",
-					"gdscript",
-				},
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },
+		opts = {
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"c_sharp",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"elixir",
+				"heex",
+				"javascript",
+				"html",
+				"angular",
+				"typescript",
+				"css",
+				"scss",
+				"markdown",
+				"rust",
+				"gdscript",
 			})
+			-- print(vim.inspect(opts)) -- Keep around as example
 		end,
 	},
 	-- Small updates and basic LSPs
