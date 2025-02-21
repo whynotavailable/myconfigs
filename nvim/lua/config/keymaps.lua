489ccdddfc503abee9f26ab56f31bd768ffbc0d4
Tom <josephdotson@belkonar.com>
2025-02-21 09:00:08 -0500

    delete scratch

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,6 @@
 local map = vim.keymap.set
 
+vim.keymap.del("n", "<leader>.")
+
 map("n", "<leader><Left>", "<C-w>h", { desc = "Go to Left Window", remap = true })
 map("n", "<leader><Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
