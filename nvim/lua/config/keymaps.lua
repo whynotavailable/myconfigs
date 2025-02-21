ba51971e7247959417022e079511cac1738330be
Tom <josephdotson@belkonar.com>
2025-02-21 08:52:10 -0500

    keymaps

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,4 @@
+local map = vim.keymap.set
+
+map("n", "<leader><Left>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<leader><Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
