a5278043a9d6e335d5a708b9745ce811b1c5fb24
Tom <josephdotson@belkonar.com>
2025-05-25 11:39:41 -0400

    updates to prompt

--- config.fish
+++ config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -p fish_function_path "$HOME/myconfigs/funcs/"
+    set fish_greeting ""
 end
--- funcs/fish_prompt.fish
+++ funcs/fish_prompt.fish
@@ -1,4 +1,9 @@
 function fish_prompt
+    set -g __fish_git_prompt_showcolorhints 1
+    set -g __fish_git_prompt_showdirtystate 1
+    set -g __fish_git_prompt_showuntrackedfiles 1
+    set -g __fish_git_prompt_char_dirtystate 1
+
     set_color $fish_color_cwd
 
     set path "$(pwd)"
@@ -9,6 +14,8 @@ function fish_prompt
     end
 
     set_color normal
+
+    fish_git_prompt
 
     set_color $fish_color_comment
     echo -n ':> '
