644a75b9c0c9b54b64fb3f1a94ed4c49c8b85ce7
Tom <josephdotson@belkonar.com>
2025-05-25 15:21:15 -0400

    git alias stuff

--- funcs/ga.fish
+++ funcs/ga.fish
@@ -1,27 +1,29 @@
-function ga
+set -l gaCommands "s c cap"
+
+function ga -V gaCommands
     argparse --min-args=1 -- $argv
     or return
 
-    if test $argv[1] = s
-        git status
-    else if test $argv[1] = c
-        argparse --min-args=2 -- $argv
-        or return
+    switch $argv[1]
+        case s
+            git status
+        case c
+            argparse --min-args=2 -- $argv
+            or return
 
-        git add -A
-        git commit -m $argv[2]
-    else if test $argv[1] = cap
-        argparse --min-args=2 -- $argv
-        or return
+            git add -A
+            git commit -m $argv[2]
+        case cap
+            argparse --min-args=2 -- $argv
+            or return
 
-        git add -A
-        git commit -m $argv[2]
-        git push
-
+            git add -A
+            git commit -m $argv[2]
+            git push
+        case '*'
+            echo "no valid command found. Valid options are ($gaCommands)"
     end
 end
-
-set -l gaCommands "s c cap"
 
 complete -c ga -f
 complete -c ga -n "not __fish_seen_subcommand_from $gaCommands" -ra "$gaCommands"
