fcde76285ddf43ae8d6a13f077c498ed7bbf5a5f
Tom <josephdotson@belkonar.com>
2025-05-31 10:17:51 -0400

    sa alias

--- config.fish
+++ config.fish
@@ -2,4 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -p fish_function_path "$HOME/myconfigs/funcs/"
     set fish_greeting ""
+
+    alias sa="tmux at"
 end
