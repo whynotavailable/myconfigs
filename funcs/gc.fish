39affc2a36d55acca17b1797ec23801a7f6cb9c6
Tom <josephdotson@belkonar.com>
2025-06-01 16:14:31 -0400

    simplified gc

--- funcs/gc.fish
+++ funcs/gc.fish
@@ -1,11 +1,9 @@
 function gc -d "Alias for git commit with -p flag to autopush"
-    argparse --min-args=1 p/push -- $argv
+    argparse --min-args=1 -- $argv
     or return
 
     git add -A
     git commit -m $argv[1]
 
-    if set -ql _flag_push
-        git push
-    end
+    git push
 end
