8e9d82e868c09b534b204020d98ddcc286a2e3c0
Tom <josephdotson@belkonar.com>
2025-08-11 06:07:54 -0400

    cleanup

---
+++ funcs/edconf.fish
@@ -0,0 +1,3 @@
+function edconf
+    curl https://gist.githubusercontent.com/whynotavailable/859541cec35bbda9b675c2e1a9a50a08/raw/c635ad8fe61cc9b5c06a22095d51a5ce3d27ab18/.editorconfig -o .editorconfig
+end
