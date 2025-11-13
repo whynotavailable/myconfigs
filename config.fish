b846bc570704e079fcf8b0dd0f2b6348f2fabfd5
tom <JosephDotson@belkonar.com>
2025-11-13 17:16:12 -0500

    tap

--- config.fish
+++ config.fish
@@ -25,8 +25,41 @@ function tap
     end
 end
 
+function boom
+    argparse f/force -- $argv
+    or return
+
+    if set -ql _flag_force
+        set -e is_boom
+    end
+
+    if test -n "$is_boom"
+        return
+    end
+
+    if test -e "./.env"
+        for row in (cat './.env')
+            set parts (string split --max=1 "=" "$row")
+            set -gx "$parts[1]" "$parts[2]"
+        end
+        echo 'envs set'
+    else
+        echo 'no .env found'
+    end
+
+    if test -e "./.init.fish"
+        source ./.init.fish
+    end
+
+    set -g is_boom boom
+end
+
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 
 # Created by `pipx` on 2025-09-22 12:50:04
 set PATH $PATH /home/tom/.local/bin
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
