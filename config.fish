4e6be469ac9dc7ab195ddb6c4be0c0c59f8de1a6
tom <JosephDotson@belkonar.com>
2026-06-01 08:08:11 -0400

    tap updated config.fish

--- config.fish
+++ config.fish
@@ -18,7 +18,7 @@ function tap
         git commit -m "$argv"
     else
         # claude -p "Generate a one or two line commit message based on current git diff, return only the message." | read msg
-        git commit -m "tap updated $(git diff --name-only --cached)"
+        git commit -m "tap updated\n$(git diff --name-only --cached)"
     end
 
     if test -n "$(git remote | grep origin)"
